86ab430edfd5d385b8b2fd143e04885da0f93690
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2023-04-30 03:17:19 +0200

    fix

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -1,6 +1,6 @@
-import SocialIcon from '@/components/social-icons'
 import Image from '@/components/Image'
 import { PageSEO } from '@/components/SEO'
+import Socials from '@/components/Socials'
 
 export default function AuthorLayout({ children, frontMatter }) {
   const { name, avatar, occupation, company, email, twitter, linkedin, github } = frontMatter
@@ -18,12 +18,7 @@ export default function AuthorLayout({ children, frontMatter }) {
             <h3>{name}</h3>
             <div>{occupation}</div>
             <div>{company}</div>
-            <div>
-              <SocialIcon kind="mail" href={`mailto:${email}`} />
-              <SocialIcon kind="github" href={github} />
-              <SocialIcon kind="linkedin" href={linkedin} />
-              <SocialIcon kind="twitter" href={twitter} />
-            </div>
+            <Socials />
           </div>
           <div>{children}</div>
         </div>
