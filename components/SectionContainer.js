d8cf96fbdcf5b67606608f24e5fed0c8a250b20c
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2023-06-03 17:24:56 +0200

    fonts

--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -1,3 +1,3 @@
 export default function SectionContainer({ children }) {
-  return <div style={{ width: '100vw', maxWidth: '70rem', padding: '0 2rem' }}>{children}</div>
+  return <div style={{ width: '100vw', maxWidth: '72rem', padding: '0 2rem' }}>{children}</div>
 }
