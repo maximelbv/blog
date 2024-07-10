4a96caa631b340f784445fc8efbe9374743ffbed
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2024-07-11 00:59:33 +0200

    fix error

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -4,14 +4,8 @@ import { siteConfig } from "@/config/site";
 
 export const runtime = "edge";
 
-const interBold = fetch(
-  new URL("../../../assets/fonts/Inter-Bold.ttf", import.meta.url)
-).then((res) => res.arrayBuffer());
-
 export async function GET(req: NextRequest) {
   try {
-    const fontBold = await interBold;
-
     const { searchParams } = req.nextUrl;
     const title = searchParams.get("title");
 
@@ -58,14 +52,6 @@ export async function GET(req: NextRequest) {
       {
         width: 1200,
         height: 630,
-        fonts: [
-          {
-            name: "Inter",
-            data: fontBold,
-            style: "normal",
-            weight: 700,
-          },
-        ],
       }
     );
   } catch (error) {
