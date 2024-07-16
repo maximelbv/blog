acf79850e6c5b6b7594af8543fefd541f57c4662
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2024-07-16 07:46:10 +0200

    remove views transition

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import { Icons } from "./icons";
 import { ModeToggle } from "./mode-toggle";
 import { MobileNav } from "./mobile-nav";
 import { Button } from "./ui/button";
-import { Link } from "next-view-transitions";
+import Link from "next/link";
 
 export function Header() {
   return (
