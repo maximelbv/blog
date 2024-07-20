239c55af8f58a3c316dfabf9daebb1f8b31b852d
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2024-07-21 00:11:12 +0200

    style callouts

--- components/callout.tsx
+++ components/callout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 interface CalloutProps {
   children?: ReactNode;
-  type?: "default" | "warning" | "danger";
+  type?: "default" | "success" | "warning" | "danger";
 }
 
 export function Callout({
@@ -14,10 +14,14 @@ export function Callout({
   return (
     <div
       className={cn(
-        "my-6 items-start rounded-md border boder-l-4 p-4 w-full dark:max-w-none",
+        "my-6 items-start rounded-md border boder-l-4 p-4 w-full dark:max-w-none bg-secondary text-primary-foreground font-medium ",
         {
-          "border-red-900 bg-red-50 dark:prose": type === "danger",
-          "border-yellow-900 bg-yellow-50 dark:prose": type === "warning",
+          "border-green-800/50 bg-green-50 dark:bg-green-50/10 text-green-600":
+            type === "success",
+          "border-yellow-800/50 bg-yellow-50 dark:bg-yellow-50/10 text-yellow-600":
+            type === "warning",
+          "border-red-800/50 bg-red-50 dark:bg-red-50/10 text-red-600":
+            type === "danger",
         }
       )}
       {...props}
