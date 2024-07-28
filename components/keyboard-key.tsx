b98c279988ada6db2679f5332604ea1b8b4786d7
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2024-07-28 18:25:06 +0200

    fix keyboard keys component & modify step component

--- components/keyboard-key.tsx
+++ components/keyboard-key.tsx
@@ -21,8 +21,8 @@ const KeyboardKey = ({ value }: { value: string }) => {
 
   return (
     <span
-      className=" h-6 text-secondary-foreground/70 inline-flex items-center justify-center bg-muted border-[1px] 
-    border-muted-foreground/30 rounded-md leading-5 py-0.5 px-1 text-[15px] font-semi-bold"
+      className=" h-6 text-secondary-foreground inline-flex items-center justify-center bg-muted border-[1px] 
+    border-border rounded-md leading-5 py-0.5 px-1 text-[15px] font-semi-bold"
     >
       {specialChar ? (
         <>
--- components/step.tsx
+++ components/step.tsx
@@ -8,10 +8,12 @@ const Step = ({ number, title, children }: StepTypes) => {
   return (
     <div className="my-8">
       <div className="flex gap-2 items-center mb-2">
-        <div className="flex items-center justify-center w-7 h-7 rounded-full bg-muted">
-          <span className="font-black text-foreground">{number}</span>
+        <div className="flex items-center justify-center w-7 h-7 rounded-full bg-secondary border-[1px] border-border">
+          <span className="font-black text-secondary-foreground">{number}</span>
         </div>
-        <span className="text-[20px] font-bold text-foreground">{title}</span>
+        <span className="text-[18px] font-bold text-secondary-foreground">
+          {title}
+        </span>
       </div>
       <div className="ml-10">{children}</div>
     </div>
