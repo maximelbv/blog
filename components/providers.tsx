86d3d720e96074a41a694fe7fe21ed38cfb4c82f
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2024-07-31 21:27:48 +0200

    change default theme

--- components/providers.tsx
+++ components/providers.tsx
@@ -8,7 +8,7 @@ export function Providers({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="dark"
       enableSystem
       disableTransitionOnChange
     >
