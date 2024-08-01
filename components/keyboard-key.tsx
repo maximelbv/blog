8982649db955657577b496b519a3f3cb3ab32a40
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2024-08-01 22:53:03 +0200

    modify keyboard keys & inline code

--- components/keyboard-key.tsx
+++ components/keyboard-key.tsx
@@ -25,7 +25,7 @@ const KeyboardKey = ({ value }: { value: string }) => {
 
   return (
     <span
-      className=" h-6 text-secondary-foreground inline-flex items-center justify-center bg-muted border-[1px] 
+      className=" h-6 text-secondary-foreground/70 inline-flex items-center justify-center bg-muted border-[1px] 
     border-border rounded-md leading-5 py-0.5 px-1 text-[15px] font-semi-bold"
     >
       {specialChar ? (
--- components/step.tsx
+++ components/step.tsx
@@ -8,14 +8,14 @@ const Step = ({ number, title, children }: StepTypes) => {
   return (
     <div className="my-6">
       <div className="flex gap-2 items-center mb-2">
-        <div className="flex items-center justify-center w-7 h-7 rounded-full bg-secondary border-[1px] border-border">
+        <div className="flex items-center justify-center w-[26px] h-[26px] rounded-full bg-secondary border-[1px] border-border">
           <span className="font-black text-secondary-foreground">{number}</span>
         </div>
-        <span className="text-[18px] font-bold text-secondary-foreground">
+        <span className="text-[16px] font-bold text-secondary-foreground">
           {title}
         </span>
       </div>
-      <div className="ml-2 md:ml-10">{children}</div>
+      <div className="ml-2">{children}</div>
     </div>
   );
 };
