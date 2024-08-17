a10b5cdd04989e5725466b363e2deaf2313de161
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2024-08-17 05:02:56 +0200

    add latest posts & design half of the cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
+import { posts } from "#site/content";
+import PostInfos from "@/components/post-infos";
+import { sortPosts } from "@/lib/utils";
+import Link from "next/link";
+
 export default function Home() {
+  const publishedPosts = sortPosts(posts.filter((post) => post.published));
+  const lastPost = publishedPosts[0];
+  const nextToLastPost = publishedPosts[1];
+
   return (
     <div>
       <div
@@ -22,16 +31,66 @@ export default function Home() {
           Latest Posts
         </span>
         <div className="flex items-end">
-          <div className="w-[210px] h-[210px] bg-secondary border-[1px] border-border rounded-[20px] -rotate-3 hover:rotate-0 mr-[-30px] transition-transform duration-500 ease-in-out"></div>
+          <div className="w-[210px] h-[210px] bg-secondary border-[1px] border-border rounded-[20px] -rotate-3 hover:rotate-0 mr-[-30px] transition-transform duration-500 ease-in-out">
+            <PostInfos
+              className=""
+              variant="small"
+              category={nextToLastPost.category}
+              date={nextToLastPost.date}
+            />
+            <span>{nextToLastPost.title}</span>
+            <span>{nextToLastPost.description}</span>
+            <div className="flex gap-2">
+              {nextToLastPost.tags &&
+                nextToLastPost.tags.map((tag) => (
+                  <span
+                    className="inline-flex items-center rounded-full border px-2.5 py-1 text-sm font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 no-underline border-transparent bg-highlighted text-muted-foreground"
+                    key={tag}
+                  >
+                    {tag}
+                  </span>
+                ))}
+            </div>
+          </div>
           <div className="relative group">
             <div
               className="absolute w-[350px] h-[350px] bg-secondary/70 rounded-[20px] rotate-[14deg] group-hover:rotate-0 transition-transform duration-500 ease-in-out"
               style={{ zIndex: 0 }}
             ></div>
-            <div
-              className="relative w-[350px] h-[350px] bg-secondary border-[1px] border-border rounded-[20px] rotate-6 hover:rotate-0 mb-16 transition-transform duration-500 ease-in-out"
-              style={{ zIndex: 1 }}
-            ></div>
+            <Link href={lastPost.slug}>
+              {" "}
+              <div
+                className="p-7 flex flex-col justify-between gap-2 relative w-[350px] h-[350px] bg-secondary border-[1px] border-border rounded-[20px] rotate-6 hover:rotate-0 mb-16 transition-transform duration-500 ease-in-out"
+                style={{ zIndex: 1 }}
+              >
+                <div className="flex flex-col gap-2">
+                  <PostInfos
+                    className=""
+                    variant="small"
+                    category={lastPost.category}
+                    date={lastPost.date}
+                  />
+                  <span className="font-dahlia text-[32px] font-bold">
+                    {lastPost.title}
+                  </span>
+                  <span className="text-foregroundAlt">
+                    {lastPost.description}
+                  </span>
+                </div>
+
+                <div className="flex gap-2">
+                  {lastPost.tags &&
+                    lastPost.tags.map((tag) => (
+                      <span
+                        className="inline-flex items-center rounded-full border px-2.5 py-1 text-sm font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 no-underline border-transparent bg-highlighted text-muted-foreground"
+                        key={tag}
+                      >
+                        {tag}
+                      </span>
+                    ))}
+                </div>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
