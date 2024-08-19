79b9ce526fbfd32a1f2e17fdf709352c1ff83d15
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2024-08-19 11:30:09 +0200

    change 404 link

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 import AnimatedButon from "@/components/animated-buton";
 
 export default function NotFound() {
-  const nav = { route: "/blog", name: "Return Home" };
+  const nav = { route: "/", name: "Return Home" };
   return (
     <div className="w-full h-[50svh] md:h-[70svh] flex flex-col items-center justify-center">
       <h1 className="text-[80px] font-bold font-dahliaBold">404</h1>
