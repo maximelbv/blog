7fe7b22e33df09cea0d2c463fd32cd4124bbde08
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2025-01-03 12:16:34 +0100

    fix snippet cards height

--- components/snippet-card.tsx
+++ components/snippet-card.tsx
@@ -15,7 +15,7 @@ const SnippetCard = ({
     <Dialog>
       <DialogTrigger className="h-full w-full">
         <div
-          className={`${className} w-full text-start flex flex-col gap-2 no-underline p-4 bg-secondary rounded-lg border-2 border-transparent min-h-fill-available`}
+          className={`${className} w-full text-start flex flex-col gap-2 no-underline p-4 bg-secondary rounded-lg border-2 border-transparent min-h-fill-available max-h-fit`}
         >
           <PostInfos
             className=""
