c12730243bdb33791974ebb5ccb6c5d703adffe9
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2025-01-05 13:49:09 +0100

    improve modals

--- components/scrollable-dialog-content.tsx
+++ components/scrollable-dialog-content.tsx
@@ -24,7 +24,7 @@ export default function ScrollableDialogContent({
       // style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
       className={cn(
         "!fixed !top-1/2 !left-1/2 !translate-x-[-50%] !translate-y-[-50%] !w-auto !max-w-none",
-        "!w-[90%] !max-w-[700px]",
+        "!w-[90%] !max-w-[850px]",
         "!h-[85svh] md:!h-[80svh] md:!max-h-[80svh]",
         "!p-0",
         "!block",
@@ -34,7 +34,7 @@ export default function ScrollableDialogContent({
       )}
     >
       <DialogHeader className="!text-start relative">
-        <DialogTitle className="!m-0 bg-background p-[20px] md:p-[50px] !pb-[15px]">
+        <DialogTitle className="!m-0 bg-background p-[20px] md:p-[75px] !pb-[15px]">
           <div className="flex flex-col gap-[15px]">
             <PostInfos
               className="mb-[-5px]"
@@ -58,7 +58,7 @@ export default function ScrollableDialogContent({
         </DialogTitle>
       </DialogHeader>
       <div
-        className="max-h-[80vh] p-[20px] md:p-[50px] !pt-0 !mt-0 "
+        className="max-h-[80vh] p-[20px] md:p-[75px] !pt-0 !mt-0 "
         onWheel={(e) => e.stopPropagation()}
       >
         <div className="h-[1px] w-full bg-border article-layout my-[15px]" />
