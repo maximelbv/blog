cde1b599c6cac6a79afc9bc09f90e5c2a6f2865f
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2025-01-25 16:47:31 +0100

    wrote cases studies

--- components/custom-image.tsx
+++ components/custom-image.tsx
@@ -5,20 +5,27 @@ import React from "react";
 interface CustomImageProps {
   href: string;
   alt?: string;
+  className?: string;
+  imgClassName?: string;
 }
 
-const CustomImage = ({ href, alt = "" }: CustomImageProps) => {
+const CustomImage = ({
+  href,
+  alt = "",
+  className,
+  imgClassName,
+}: CustomImageProps) => {
   if (!href) return <ImagePlaceholder />;
 
   return (
-    <div className="article-layout m-auto">
+    <div className={`${className} article-layout m-auto`}>
       <Image
         src={href}
         alt={alt}
         width={0}
         height={0}
         sizes="(max-width: 700px) 100vw, 700px"
-        className="w-full h-auto rounded-[6px]"
+        className={`w-full h-auto rounded-[6px] ${imgClassName}`}
       />
     </div>
   );
