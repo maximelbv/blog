27616f0d3dfcf5377056c434dc2c744bb7170bc4
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2025-03-09 02:25:17 +0100

    implement showcase menu

--- components/latest-projects.tsx
+++ components/latest-projects.tsx
@@ -5,6 +5,7 @@ import { ARTICLE_TYPE_CONSTANTS } from "@/constants/article-type-constants";
 import { sortPosts } from "@/lib/utils";
 import CustomLink from "./custom-link";
 import ProjectCard from "./project-card";
+import ShowcaseMenu from "./ShowcaseMenu";
 
 export default async function LatestProjects() {
   const publishedPosts = sortPosts(
@@ -15,6 +16,10 @@ export default async function LatestProjects() {
       .slice(0, 3)
   );
 
+  const mappedProjectsToShowcaseMenuProps = publishedPosts.map((post) => {
+    return { link: post.slug, text: post.title, image: post.image };
+  });
+
   return (
     <div className="flex flex-col gap-[15px]">
       <FadeIn>
@@ -23,13 +28,18 @@ export default async function LatestProjects() {
         </span>
       </FadeIn>
 
-      <div className="grid row-gap w-full grid-cols-1 md:grid-cols-3 gap-6">
+      {/* <div className="grid row-gap w-full grid-cols-1 md:grid-cols-3 gap-6">
         {publishedPosts.map((post, index) => (
           <FadeIn key={index} delay={(index + 1) * 0.2}>
             <ProjectCard key={post.slug} project={post} />
           </FadeIn>
         ))}
-      </div>
+      </div> */}
+      <ShowcaseMenu
+        items={mappedProjectsToShowcaseMenuProps}
+        className="mb-4 font-dahliaLight"
+        itemClassName="!px-4 !py-6"
+      />
       <div className="flex justify-end items-center">
         <FadeIn>
           <CustomLink href="/projects" className="text-[18px]">
